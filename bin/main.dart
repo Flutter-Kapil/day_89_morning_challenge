926ef3cdd3feb9eb447849335f9153c2bfe30e7f
Kapil Jhajhria <jhajhria44@gmail.com>
2020-02-27 10:46:02 +0530

    time up

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,32 @@
 // Write a function to merge two sorted lists.
 
 void main() {
+  List<int> a = [1,2,4,9];
+  List<int> b = [5,6,8];
 
 }
+
+List<int> mergeList(List<int> a, List<int> b){
+  List<int> big=[];
+  List<int> small=[];
+  if(a.length>b.length)
+    big=a;
+  else
+    big=b;
+  //---------
+  for(int i=0;i<big.length;i++){
+    if(big[i]>small[0]){
+      //add that element from small list of big list
+      big.insertAll(i-1,small);
+    }
+  }
+return big;
+}
+
+List mergeList2(List a, List b){
+  a.addAll(b);
+  a.sort();
+  return a;
+}
+
 
